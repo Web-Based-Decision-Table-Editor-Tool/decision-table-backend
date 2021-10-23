870dd25d640019493f087e07ab186e8269b3fc36
Cyril Saidane <cs.saidane@gmail.com>
2021-10-23 16:36:08 -0400

    adding rules: example code

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,12 @@ app.get('/', (req: Request, res: Response)=>{
     res.send('Server listening on port ' + port);
 });
 
+//Incorect syntax, this is GET syntax (demo purposes only)
+app.post('/rule/add/:tableid', (req: Request, res: Response)=>{
+    //Calling addRule service to add rule to table, using data from this post request
+    res.send('Server listening on port ' + port);
+});
+
 app.listen(port, ()=> {
     console.log('Server successfully started on port ' + port);
 })
---
+++ src/service/ruleService.ts
@@ -0,0 +1,8 @@
+
+
+
+const addRule = function (object) {
+    //take table ID, Load it from memory
+    // add rule
+    //exit
+}
