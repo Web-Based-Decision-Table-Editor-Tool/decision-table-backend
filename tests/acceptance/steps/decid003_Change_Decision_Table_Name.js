ac3f5643b25cd68fc9215764246d5f0c195c3eb6
Ibrahim T. Miraj <zerobottles@outlook.com>
2022-01-22 16:10:05 -0500

    initial decid003 work

--- tests/acceptance/steps/decid003_Change_Decision_Table_Name.js
+++ tests/acceptance/steps/decid003_Change_Decision_Table_Name.js
@@ -8,4 +8,6 @@ const host = 'localhost:3000';
 
 var decid003Response;
 
-When('I change the name of decision table with id {string} to the new name {')
+When('I change the name of decision table with id {string} to the new name of {string}', async function (dec_tag, new_dec_name) {
+    
+})
