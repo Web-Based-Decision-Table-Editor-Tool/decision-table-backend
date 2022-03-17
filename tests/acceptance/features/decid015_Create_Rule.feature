f40b8d065cd06e6351fe9a9c29aff372f67e3fe3
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2022-03-17 16:39:07 -0400

    add acceptance tests for decid015

---
+++ tests/acceptance/features/decid015_Create_Rule.feature
@@ -0,0 +1,67 @@
+@CreateRuleFeature
+Feature: Create Rule Story
+
+As a developer who uses the Decision_Table_Editor_Cloud_Services to build applications
+I want to create a new rule in a decision table
+So that I can add rules which link conditions and actions in my decision table
+
+Acceptance Tests
+
+##
+## Note:
+##
+## The Step definitions are expected to manage a look up table 
+## which associates the string dec_tag with a decision table identifier from the
+## Decision_Table_Cloud_Sevices 
+##
+Scenario Outline: Create a rule (Normal Flow)
+
+Given I am connected to the Decision_Table_Editor_Cloud_Services
+And I have created decision table named <dec_name> identified as <dec_tag>
+And I have a condition in table identified as <dec_tag> named <con_name> with type <con_type> and values <con_vals>
+And I have an action in table <dec_tag> of type <action_type> named <action_name> with action values: <action_values>
+When I create a rule in table <dec_tag> with condition <con_name> and condition value <con_num>, and action <action_name> and action value <act_num>
+Then I receive a unique rule identifier
+And created rule has specified condition with value <con_num> and action with value <act_num>
+And I receive an error code as <rule_response_code> for rule creation
+
+Examples:
+
+| dec_name       | dec_tag | con_name   | con_type   | con_vals                         |  rule_response_code  |  action_name       | action_type   | action_values   | con_num             | act_num          |
+| "RobSab01"     | "dt_1"  | "Region"   | "text"     | "Domestic,International"         |  201                 | "dec_action1"      | "boolean"     | "Ship, No Ship" | "condition-value-1" | "action-value-2" | 
+| "RobSab02"     | "dt_2"  | "Ship"     | "boolean"  | "Go,No Go"                       |  201                 | "dec_action2"      | "text"        | "Ship, No Ship" | "condition-value-2" | "action-value-1" |
+| "RobSab03"     | "dt_3"  | "Weight"   | "numeric"  | "<5,>0"                          |  201                 | "dec_action3"      | "boolean"     | "Go, No Go"     | "condition-value-1" | "action-value-2" |
+
+Scenario Outline: Invalid condition for rule (Error Flow)
+
+Given I am connected to the Decision_Table_Editor_Cloud_Services
+And I have created decision table named <dec_name> identified as <dec_tag>
+When I create a rule in table <dec_tag> with condition that does not exist
+Then I receive an error code as <rule_response_code> for rule creation
+Examples:
+
+| dec_name       | dec_tag | rule_response_code| Comment                 |
+| "RobSab01"     | "dt_4"  | 400               | non existing condition  |
+
+Scenario Outline: Invalid condition for rule (Error Flow)
+
+Given I am connected to the Decision_Table_Editor_Cloud_Services
+And I have created decision table named <dec_name> identified as <dec_tag>
+When I create a rule in table <dec_tag> with action that does not exist
+Then I receive an error code as <rule_response_code> for rule creation
+Examples:
+
+| dec_name       | dec_tag | rule_response_code| Comment                 |
+| "RobSab01"     | "dt_5"  | 400               | non existing action     |
+
+# Scenario Outline: Create a rule when no table exists (Error Flow)
+
+# Given I am connected to the Decision_Table_Editor_Cloud_Services
+# When I create a condition in table identified as <dec_tag> named <con_name> with type <con_type> and values <con_vals>
+# Then I receive an error code as <con_response_code> for condition creation
+# And I receive an error message as <err_msg> for condition creation
+
+# Examples:
+
+# | dec_name       | dec_note          | dec_tag | con_name   | con_type   | con_vals                         | err_msg                                                       | con_response_code | Comment |
+# | "RobSab01"     | "NoteRS01"        | "dt_X"  | "Region"   | "text"     | "Domestic,International"         |  "No table with matching Id exists, adding condition failed"  | 404               | creating a condition in a non-existent table  |
---
+++ tests/acceptance/steps/decid015_Create_Rule.js
@@ -0,0 +1,117 @@
+const chai = require("chai");
+const expect = require("chai").expect;
+const chaiHttp = require("chai-http");
+const { Given, When, Then, Before } = require("@cucumber/cucumber");
+const { resetFileStore, shutdown, startServer } = require('./TestUtils');
+
+chai.use(chaiHttp);
+const host = 'localhost:3000';
+
+var decid015Response;
+var condition;
+var action;
+var rule;
+
+/*
+ Background: 
+ 'Before' normally executes before every scenario of every feature file
+ Therefore, tags is added so that 'Before' is only executed for the features identified in tags
+
+ Behaviour: 
+ The logic resets the filestore, shutsdown the server and restarts the server. 
+ */
+ let executeOnce = false;
+Before({tags: "@CreateRuleFeature"}, async function () {
+    try {
+        // Only execute this logic before the first scenario
+        if(!executeOnce){
+            await resetFileStore();
+            await shutdown();
+            await startServer();
+            executeOnce = true;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+Given('I have a condition in table identified as {string} named {string} with type {string} and values {string}', async function (dec_tag, con_name, con_type, con_vals) {
+    decid015Response = await chai
+        .request(host)
+        .post("/condition")
+        .send({ tableId: dec_tag, name: con_name, type: con_type, valueList: con_vals.split(',') });
+        
+    expect(decid015Response.body.id).to.not.equal(null);
+    expect(decid015Response.body.id).to.not.equal(undefined);
+    condition = decid015Response.body;
+  });
+
+Given('I have an action in table {string} of type {string} named {string} with action values: {string}', async function (dec_tag, action_type, action_name, action_values) {
+    vals = action_values.split(",");
+    let reqBody = {
+        name: action_name,
+        type: action_type,
+        tableId: dec_tag, 
+        valueList: vals
+    };
+    decid015Response = await chai.request(host).post('/action').send(reqBody);
+    expect(decid015Response.body).to.have.property("id")
+    actionID = decid015Response.body.id
+    reqBody = {
+        tableId : dec_tag,
+        actionId: actionID
+    }
+    // Write code here that turns the phrase above into concrete actions
+    decid015Response = await chai
+        .request(host)
+        .get("/action/" + actionID).send(reqBody);
+    
+    expect(decid015Response.body.name).to.equal(action_name)
+
+    action = decid015Response.body;
+  });
+
+When('I create a rule in table {string} with condition {string} and condition value {string}, and action {string} and action value {string}', async function (dec_tag, con_name, con_num, action_name, action_num) {
+    const reqBody = {
+        tableId: dec_tag,
+        conditions: [{ id: condition.id, valueid: con_num}],
+        actions: [{ id: action.id, valueid: action_num}]
+    }
+    decid015Response = await chai.request(host).post('/rule').send(reqBody);
+    rule = decid015Response.body
+});
+
+Then('I receive a unique rule identifier', function () {
+    expect(rule).to.haveOwnProperty("id");
+  });
+
+Then('created rule has specified condition with value {string} and action with value {string}', function (con_num, action_num) {
+    expect(rule.conditions[0].item.id).to.equal(condition.id)
+    expect(rule.conditions[0].valueid).to.equal(con_num)
+    expect(rule.actions[0].item.id).to.equal(action.id)
+    expect(rule.actions[0].valueid).to.equal(action_num)
+  });
+
+Then('I receive an error code as {int} for rule creation', function (code) {
+    expect(decid015Response).to.have.status(code);
+});
+
+When('I create a rule in table {string} with condition that does not exist', async function (dec_tag) {
+    const reqBody = {
+        tableId: dec_tag,
+        conditions: [{ id: "invalid-id", valueid: "condition-value-1"}],
+        actions: [{ id: "invalid-id", valueid: "action-value-1"}]
+    }
+    decid015Response = await chai.request(host).post('/rule').send(reqBody);
+    console.log(decid015Response.body)
+});
+
+When('I create a rule in table {string} with action that does not exist', async function (dec_tag) {
+    const reqBody = {
+        tableId: dec_tag,
+        conditions: [{ id: "invalid-id", valueid: "condition-value-1"}],
+        actions: [{ id: "invalid-id", valueid: "action-value-1"}]
+    }
+    decid015Response = await chai.request(host).post('/rule').send(reqBody);
+    console.log(decid015Response.body)
+});
