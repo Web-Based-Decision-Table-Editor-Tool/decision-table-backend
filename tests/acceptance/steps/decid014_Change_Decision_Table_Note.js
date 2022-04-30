76472c764185172ec698745c66f248666e08359c
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2022-05-01 02:02:22 +0500

    decid014: update acceptance tests

--- tests/acceptance/steps/decid014_Change_Decision_Table_Note.js
+++ tests/acceptance/steps/decid014_Change_Decision_Table_Note.js
@@ -1,17 +1,21 @@
 const chai = require("chai");
 const expect = require("chai").expect;
 const chaiHttp = require("chai-http");
-const { When, Then, Before } = require("@cucumber/cucumber");
-
+const { When, Then, Given } = require("@cucumber/cucumber");
+const { createDecTable } = require("./TestUtils.js")
 
 chai.use(chaiHttp);
 const host = 'localhost:3000';
 
 var decid014Response;
+var tableId;
 
+Given('I have created decision table named {string} to update note', async function (dec_name) {
+    tableId = await createDecTable(dec_name,"");
+});
 
-When('I change the note of Decision Table {string} to {string}', async function(dec_tag, new_dec_note) {
-    decid014Response = await chai.request(host).put("/table/note").send({ id: dec_tag, note: new_dec_note});
+When('I change the note of Decision Table to {string}', async function(new_dec_note) {
+    decid014Response = await chai.request(host).put("/table/note").send({ id: tableId, note: new_dec_note});
 });
 
 Then('I receive the new note matching {string}', function(new_dec_note) {
