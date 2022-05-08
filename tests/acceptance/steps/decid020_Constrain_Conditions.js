1e317e3c14c236f8a819cf0c0e1e32034c31149d
Ibrahim T. Miraj <zerobottles@outlook.com>
2022-05-08 14:50:11 -0400

    all constraint and conditions working

--- tests/acceptance/steps/decid020_Constrain_Conditions.js
+++ tests/acceptance/steps/decid020_Constrain_Conditions.js
@@ -23,6 +23,8 @@ var con_type = 'boolean'
 var con_vals = 'Go,NoGo'
 const maxConditionsInTable = data.maxConditionsInTable;
 const maxActionsInTable = data.maxActionsInTable;
+const maxRulesInTable = data.maxRulesInTable;
+
 
   
 Given('I have created decision table named {string} for testing condition constraint', async function(dec_name) {
@@ -36,6 +38,7 @@ Given('I have created the maximum amount of conditions allowed', async function 
         .request(host)
         .post("/condition")
         .send({ tableId: tableId, name: con_name, type: con_type, valueList: con_vals.split(',') });
+        createdCondition = decid020Response.body;
     }
 
   });
@@ -49,6 +52,19 @@ Given('I have created the maximum amount of action allowed', async function () {
         .send({ tableId: tableId, name: act_name, type: con_type, valueList: con_vals.split(',') });
     }
 
+    createdAction = decid020Response.body;
+
+  });
+
+  Given('I have created the maximum amount of rules allowed', async function () {
+
+    for(let i = 0; i < maxRulesInTable; i++) {
+      decid020Response = await chai
+      .request(host)
+      .post("/rule")
+      .send({ tableId: tableId, conditions: [{id: createdCondition.id, valueid: "condition-value-1"}], actions: [{id: createdAction.id, valueid: "action-value-1"}]});    
+    }
+
   });
 
 When('I create another condition', async function () {
@@ -58,8 +74,6 @@ When('I create another condition', async function () {
   .post("/condition")
   .send({ tableId: tableId, name: con_name, type: con_type, valueList: con_vals.split(',') });  
 
-    createdCondition = decid020Response.body.action;
-    
   });
 
   When('I create another action', async function () {
@@ -68,10 +82,17 @@ When('I create another condition', async function () {
     .request(host)
     .post("/action")
     .send({ tableId: tableId, name: act_name, type: con_type, valueList: con_vals.split(',') });  
-  
-      createdCondition = decid020Response.body.action;
       
   });
+
+  When('I create another rule', async function () {
+    
+    decid020ResponseRule = await chai
+    .request(host)
+    .post("/rule")
+    .send({ tableId: tableId, conditions: [{id: createdCondition.id, valueid: "condition-value-1"}], actions: [{id: createdAction.id, valueid: "action-value-1"}]});  
+  });
+
 
   Then('I receive an error code as {int} for creating a condition', function (condition_response_code) {
     expect(decid020ResponseCondition.status).to.equal(condition_response_code);
@@ -79,5 +100,9 @@ When('I create another condition', async function () {
 
   Then('I receive an error code as {int} for creating a action', function (action_response_code) {
     expect(decid020ResponseAction.body.status).to.equal(action_response_code);
+  });
+
+  Then('I receive an error code as {int} for creating a rule', function (rule_response_code) {
+    expect(decid020ResponseRule.status).to.equal(rule_response_code);
   });
 
