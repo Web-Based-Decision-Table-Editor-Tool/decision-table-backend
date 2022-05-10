33310105fbb82cd83fa9b35dc2afea07452e215d
Talha Riaz <talha.riaz@mail.mcgill.ca>
2022-05-11 02:50:50 +0500

    fix unit test

--- src/persistence/__mocks__/decision-table.persistence.ts
+++ src/persistence/__mocks__/decision-table.persistence.ts
@@ -1,3 +1,4 @@
+import { decisionTableController } from "../../rest-controller/decision-table.controller";
 import { DecisionTable } from "../../types/decision-table";
 
 const mock = jest.fn().mockImplementation(() => (
@@ -11,6 +12,10 @@ const mock = jest.fn().mockImplementation(() => (
       },
       async saveTable(table: DecisionTable){
         // empty implementation
+      },
+      loadTable(tableId: string): DecisionTable | null {
+        const table = "{\"id\":\"5183b4cf-0b51-422c-a815-ce8409ba18f8\",\"name\":\"First\",\"note\":\"Thisisastarter\",\"email\":\"\",\"lastUsedDate\":\"2022-05-10T21:45:44.905Z\",\"actions\":[],\"conditions\":[],\"rules\":[]}"
+        return JSON.parse(table);
       },
       async changeTableNameById(id: string, newName: string): Promise<string>{
         return "New table name"
